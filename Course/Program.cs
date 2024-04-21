e2be67aa69b5e9eb246cc621e22424742ec9df4f
Italo Goes <italogoes85@gmail.com>
2024-04-21 16:16:44 -0300

    first commit

---
+++ Course/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+
+namespace Course;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] v = Console.ReadLine().Split(' ');
+
+        string a = v[0];
+        string b = v[1];
+        string c = v[2];
+
+        Console.WriteLine($"{a} {b} {c}");
+    }
+}
+
